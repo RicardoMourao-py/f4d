ac716b3e703bafc2565e3e96a356f0cf870b3a76
jonasbp <jonasbonfapelegrina@gmail.com>
2021-06-08 22:13:35 -0300

    textos melhorados

--- script/data.js
+++ script/data.js
@@ -1,32 +1,32 @@
 let dados = {
     "propostas": {
     "p1": {
-        "nome": "Padaria",
+        "nome": "Acessórios para festas",
         "valor": "100,00",
         "proposta1": "Tenho um portal com anunciantes no ramo de festas e nele uma ferramenta chamada cotação express.",
         "proposta2": "O objetivo deste projeto é aperfeiçoar este sistema.",
         "proposta3": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados"
     },
     "p2":{
-        "nome": "Açougue",
+        "nome": "Construtora",
         "valor": "10,00",
         "proposta1": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados",
         "proposta2": "O objetivo deste projeto é aperfeiçoar este sistema.",
         "proposta3": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados"
     },
     "p3":{
-        "nome": "Material de Construção",
+        "nome": "Padaria",
         "valor": "100,00",
         "proposta1": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados",
         "proposta2": "O objetivo deste projeto é aperfeiçoar este sistema.",
         "proposta3": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados"
     },
     "p4":{
-        "nome": "Pet Shop",
-        "valor": "50,00",
-        "proposta1": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados",
-        "proposta2": "O objetivo deste projeto é aperfeiçoar este sistema.",
-        "proposta3": "O visitante que vai organizar uma festa, preenche um formulario único com seus dados e os dados da festa, e essa cotação é enviada por e-mail aos anunciantes selecionados"
+        "nome": "Aplicativo para Pet Shop",
+        "valor": "500,00",
+        "proposta1": "Gostaria de criar um aplicativo para melhorar meu sistema de agendamento de horário.",
+        "proposta2": "Hoje em dia o processo é todo manual e gostaria de deixar mais prático para meus clientes.",
+        "proposta3": "Estou em uma fase bem inicial de pensamento sobre o aplicativo e gostaria de mais ideias para deixarmos o aplicativo ótimo para meu cliente"
     },
     "p5":{
         "nome": "Loja de doces",
